96cd15fc0c5325b06315a8cebc3bbe927ad309e3
Reginaldo Santos <reginaldosantos.br@gmail.com>
2025-01-05 13:24:30 -0400

    add Footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <footer className="bg-gray-800 text-white">
+            <div className="container mx-auto px-6 py-3">
+                <p className="text-center">
+                    Portfólio - Do Zero ao React em 2025. Todos os direitos reservados.
+                </p>
+            </div>
+        </footer>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
@@ -23,6 +24,7 @@ export default function Home() {
           </div>
         </main>
       </div>
+      <Footer />
     </>
   );
 }
