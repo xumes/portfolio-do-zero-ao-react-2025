8939e2efab4025c9938d9c3abe3dfb1798c29cca
Reginaldo Santos <reginaldosantos.br@gmail.com>
2025-01-05 15:05:09 -0400

    add chadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-      <div className="flex flex-col items-center justify-center  py-2">
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
         <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
           <h1 className="text-6xl">Bem Vindo ao meu <span className="text-blue-600">Portfólio!</span></h1>
           <p className="mt-3 text-2xl">
