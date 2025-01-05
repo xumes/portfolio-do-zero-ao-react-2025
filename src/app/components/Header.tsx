c6ff36ab9c16b29a47335c01fda9dcd4d061ff7e
Reggie <reginaldo.santos@redspace.com>
2025-01-05 12:31:18 -0400

    add Header

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header() {
+    return (
+        <header className="bg-white shadow-md">
+            <nav className="container mx-auto px-6 py-3">
+                <ul className="flex justify-between text-gray-800">
+                    <li>Meu Portfólio</li>
+                    <li>Projetos</li>
+                    <li>Admin</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,27 @@
+import Header from "./components/Header";
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl">Bem Vindo ao meu <span className="text-blue-600">Portfólio!</span></h1>
-        <p className="mt-3 text-2xl">
-          Comece a construir o seu portfólio profissional hoje!
-        </p>
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <div className="p-6 mt-6 text-left border rounded-xl hover:text-blue-600 focus:text-blue-600">
-            <h2 className="text-2xl font-semibold">Ver Portfólio</h2>
-            <p className="mt-4 text-xl">Explore os projetos e trabalhos realizados.</p>
+    <>
+      <Header />
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+          <h1 className="text-6xl">Bem Vindo ao meu <span className="text-blue-600">Portfólio!</span></h1>
+          <p className="mt-3 text-2xl">
+            Comece a construir o seu portfólio profissional hoje!
+          </p>
+          <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
+            <div className="p-6 mt-6 text-left border rounded-xl hover:text-blue-600 focus:text-blue-600">
+              <h2 className="text-2xl font-semibold">Ver Portfólio</h2>
+              <p className="mt-4 text-xl">Explore os projetos e trabalhos realizados.</p>
+            </div>
+            <div className="p-6 mt-6 text-left border rounded-xl hover:text-blue-600 focus:text-blue-600">
+              <h2 className="text-2xl font-semibold">Área Administrativa</h2>
+              <p className="mt-4 text-xl">Gerencie seu portfólio (requer login).</p>
+            </div>
           </div>
-          <div className="p-6 mt-6 text-left border rounded-xl hover:text-blue-600 focus:text-blue-600">
-            <h2 className="text-2xl font-semibold">Área Administrativa</h2>
-            <p className="mt-4 text-xl">Gerencie seu portfólio (requer login).</p>
-          </div>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
+    </>
   );
 }
