1c9b66938f69bfa58d8dd3117c782b0f6feecbbe
Reginaldo Santos <reginaldosantos.br@gmail.com>
2025-01-11 12:23:04 -0400

    create addPortfolioItem action

---
+++ src/app/actions/portfolioActions.ts
@@ -0,0 +1,38 @@
+"use server"
+
+import prisma from "../../lib/prisma"
+
+type AddPortfolioInput = {
+    title: string
+    description: string
+    imageUrl: string
+    tags: string[]
+    projectUrl?: string
+    githubUrl?: string
+}
+
+export async function addPortfolioItem(data: AddPortfolioInput) {
+    try {
+        const portfolioItem = prisma.portfolioItem.create({
+            data: {
+                title: data.title,
+                description: data.description,
+                imageUrl: data.imageUrl,
+                projectUrl: data.projectUrl,
+                githubUrl: data.githubUrl,
+                tags: data.tags,
+            }
+        })
+
+        return {
+            sucess: true, data: portfolioItem
+        }
+    }
+    catch (error) {
+        console.error("PortfolioActions: Error adding portfolio item: ", error)
+        return {
+            success: false,
+            error: "Erro ao adicionar item de Portfólio"
+        }
+    }
+}
